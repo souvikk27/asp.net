462c4cc28aa6ad4b63513b83122ddec421012d4b
souvikk27 <souvikvip@outlook.com>
2022-12-18 21:43:09 +0530

    ViewState demo

---
+++ ViewStateDemo/WebApplication1/WebForm1.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            
+        </div>
+        User Name :-<asp:TextBox ID="TextBox1" runat="server" style="margin-left: 17px" Width="149px"></asp:TextBox>
+        <br />
+         Password&nbsp;&nbsp; :-<asp:TextBox ID="TextBox2" runat="server" style="margin-left: 19px" Width="149px"></asp:TextBox>
+        <p>
+            <asp:Button ID="Button1" runat="server" Text="Submit" Width="93px" OnClick="Button1_Click" />
+            <asp:Button ID="Button2" runat="server" style="margin-left: 13px" Text="Restore" Width="95px" OnClick="Button2_Click" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ ViewStateDemo/WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //Value of Textbox1 and TectBox2 is assigin on the ViewState  
+            ViewState["name"] = TextBox1.Text;
+            ViewState["password"] = TextBox2.Text;
+            //after clicking on Button TextBox value Will be Cleared  
+            TextBox1.Text = TextBox2.Text = string.Empty;
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            //If ViewState Value is not Null then Value of View State is Assign to TextBox  
+            if (ViewState["name"] != null)
+            {
+                TextBox1.Text = ViewState["name"].ToString();
+            }
+            if (ViewState["password"] != null)
+            {
+                TextBox2.Text = ViewState["password"].ToString();
+            }
+        }
+    }
+}
